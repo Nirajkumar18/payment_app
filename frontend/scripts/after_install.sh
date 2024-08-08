5bc5ceeee0d2807c5d9d4bb927e3cd715313ac70
Niraj kumar <kumarniraj6205@gmail.com>
2024-08-08 15:24:57 +0530

    Update after_install.sh

--- frontend/scripts/after_install.sh
+++ frontend/scripts/after_install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-set -e
-set -x
-
-# Ensure Node.js and npm are installed
-if ! command -v npm &> /dev/null
-then
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-fi
-
 # Navigate to the app directory
 cd /var/www/html/app
 
